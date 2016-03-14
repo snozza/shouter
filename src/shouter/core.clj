310dd984935c015bf5f7919a84ffb339c94372d4
snozza <andrewcsnead@gmail.com>
2016-03-14 16:41:01 +0000

    add basic restful api

---
+++ src/shouter/core.clj
@@ -0,0 +1,25 @@
+(ns shouter.core
+  (:use [compojure.core :only (defroutes)]
+        [ring.adapter.jetty :as ring])
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [shouter.controllers.shouts :as shouts]
+            [shouter.views.layout :as layout]
+            [shouter.models.migration :as schema])
+  (:gen-class))
+
+(defroutes routes
+  shouts/routes
+  (route/resources "/")
+  (route/not-found (layout/four-oh-four)))
+
+(def application (handler/site routes))
+
+(defn start [port]
+  (run-jetty application {:port port
+                          :join? false}))
+
+(defn -main []
+  (schema/migrate)
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
+    (start port)))
---
+++ src/shouter/views/shouts.clj
@@ -0,0 +1,23 @@
+(ns shouter.views.shouts
+  (:use [hiccup.core :only (h)]
+        [hiccup.form :only (form-to label text-area submit-button)])
+  (:require [shouter.views.layout :as layout]))
+
+(defn shout-form []
+  [:div {:id "shout=form" :class "sixteen columsn alpha omega"}
+   (form-to [:post "/"]
+            (label "shout" "What do you want to SHOUT?")
+            (text-area "shout")
+            (submit-button "SHOUT!"))])
+
+(defn display-shouts [shouts]
+  [:div {:class "shouts sixteen columns alpha omega"}
+   (map
+    (fn [shout] [:h2 {:class "shout"} (h (:body shout))])
+    shouts)])
+
+(defn index [shouts]
+  (layout/common "SHOUTER"
+                 (shout-form)
+                 [:div {:class "clear"}]
+                 (display-shouts shouts)))
